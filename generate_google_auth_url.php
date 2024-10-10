d952af7bf548883f12c5ec652cad59852776e5c2
Ja9 <116960057+ja9karla@users.noreply.github.com>
2024-10-10 20:02:21 +0800

    no secret key generate_google_auth_url.php

--- generate_google_auth_url.php
+++ generate_google_auth_url.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$clientID = '933379690745-04auqcdqbttk2sfplrhr4bq1ues1hkp6.apps.googleusercontent.com';
-$clientSecret = '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';
+$clientID = 'email auth';
+$clientSecret = 'password mue';
 $redirectUri = 'http://localhost/mindsoothe(1)/google_callback.php';
 
 $client = new Google_Client();
